0c77a6c856c1961422574e7de5ee76771fdd4967
Victoria Blackburn <victoria@sixtwo.tech>
2022-03-25 10:39:43 +0000

    Adds votes to local storage

--- src/JokeList.js
+++ src/JokeList.js
@@ -12,16 +12,7 @@ class JokeList extends Component {
   constructor(props) {
     super(props);
     this.state = { jokes: JSON.parse(window.localStorage.getItem("jokes")) || "[]" };
-  }
-
-  handleVote(id, delta) {
-    this.setState(
-      st => ({
-        jokes: st.jokes.map(j => 
-          j.id === id ? {...j, votes: j.votes + delta} : j
-        )
-      })
-    )
+    this.handleClick = this.handleClick.bind(this);
   }
   
   componentDidMount() {
@@ -37,11 +28,26 @@ class JokeList extends Component {
       });
       jokes.push({ id: uuidv4(), text: res.data.joke, votes: 0 });
     }
-    this.setState({ jokes: jokes });
-    window.localStorage.setItem(
-      "jokes",
-      JSON.stringify(jokes)
+    this.setState(st => ({
+      jokes: [...st.jokes, ...jokes]
+    }),
+      () => window.localStorage.setItem("jokes", JSON.stringify(this.state.jokes))
+    );
+  }
+  
+  handleVote(id, delta) {
+    this.setState(
+      st => ({
+        jokes: st.jokes.map(j => 
+          j.id === id ? {...j, votes: j.votes + delta} : j
+        )
+      }),
+      () => window.localStorage.setItem("jokes", JSON.stringify(this.state.jokes))
     )
+  }
+
+  handleClick() {
+    this.getJokes();
   }
 
   render() {
@@ -52,7 +58,7 @@ class JokeList extends Component {
           <span>Dad</span> Jokes
           </h1>
           <img src="https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg" alt="" />
-          <button className="JokeList-getmore">New Jokes</button>
+          <button className="JokeList-getmore" onClick={this.handleClick}>New Jokes</button>
         </div>
         <div className="JokeList-jokes">
           {this.state.jokes.map(j => (
