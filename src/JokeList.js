8a5a1c9ebf6dc5d7b8ba6c313d7c439103b65ea1
Victoria Blackburn <victoria@sixtwo.tech>
2022-03-28 09:28:09 +0100

    Sorts jokes by vote value

--- src/JokeList.js
+++ src/JokeList.js
@@ -76,6 +76,8 @@ class JokeList extends Component {
         </div>
       );
     }
+
+    let jokes = this.state.jokes.sort((a, b) => b.votes - a.votes);
     return (
       <div className="JokeList">
         <div className="JokeList-sidebar">
@@ -86,7 +88,7 @@ class JokeList extends Component {
           <button className="JokeList-getmore" onClick={this.handleClick}>New Jokes</button>
         </div>
         <div className="JokeList-jokes">
-          {this.state.jokes.map(j => (
+          {jokes.map(j => (
             <Joke
               key={j.id}
               votes={j.votes}
